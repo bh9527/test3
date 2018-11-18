c0d760418fe68bb20cefa95e73b52772b06e64ad
xml <1072387550@qq.com>
2018-11-18 20:04:55 +0800

    第一次提交

---
+++ GIT/src/ALL.java
@@ -0,0 +1,9 @@
+public class ALL {
+    public static void main(String[] args) {
+
+        System.out.println("呵呵,去洗澡");
+        System.out.println("caonima");
+        System.out.println("您");
+
+    }
+}
---
+++ GIT/src/find2.java
@@ -0,0 +1,7 @@
+public class find2 {
+    public static void main(String[] args) {
+
+        System.out.println("呵呵,去洗澡");
+
+    }
+}
