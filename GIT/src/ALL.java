5a9e3b48800f7b51e682eadd8cbf8504d8810d02
xml <1072387550@qq.com>
2018-11-19 08:58:12 +0800

    第er次提交

--- GIT/src/ALL.java
+++ GIT/src/ALL.java
@@ -2,8 +2,8 @@ public class ALL {
     public static void main(String[] args) {
 
         System.out.println("呵呵,去洗澡");
-        System.out.println("caonima");
-        System.out.println("您");
+        System.out.println("您啊");
+        System.out.println("给");
         System.out.println("gouzei");
 
     }
